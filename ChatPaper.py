dd0add65507f1a036c18fd6b5cc651714c3e1406
Li Zhu <z@zhuli.name>
2023-04-20 00:10:27 -0400

    Improve token management for GPT API and handle long PDFs with text chunking
    - Added split_text function to divide the text from the PDF into smaller chunks
    - Updated ChatPaper function to process text chunks sequentially

--- ChatPaper.py
+++ ChatPaper.py
@@ -1,40 +1,57 @@
-
 import fitz
 
 def pdf2txt(stream):
-    with fitz.open(stream=stream, filetype="pdf") as doc: # open document
+    with fitz.open(stream=stream, filetype="pdf") as doc:  # open document
         text_list = [page.get_text() for page in doc]
     return "\n\n".join(text_list)
 
-import openai 
-import ChatPaper_config # a simple python file containing 
-                     # a variable called openai_api_key
+def split_text(text, max_length=4000):
+    words = text.split()
+    chunks = []
+    current_chunk = []
+
+    for word in words:
+        if len(" ".join(current_chunk)) + len(word) <= max_length:
+            current_chunk.append(word)
+        else:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = [word]
+    chunks.append(" ".join(current_chunk))
+
+    return chunks
+
+import openai
+import ChatPaper_config  # a simple python file containing
+# a variable called openai_api_key
 openai.api_key = ChatPaper_config.openai_api_key
 
 import funix
 
-from funix.widget.builtin import BytesFile 
+from funix.widget.builtin import BytesFile
 
 @funix.funix()
 def ChatPaper(
     PDFBytes: BytesFile,
-    prompt_template: str="The following is a paper about {article}", 
-    system_message: str="",
-    ) -> str: 
+    prompt_template: str = "The following is part of a paper about {article}",
+    system_message: str = "",
+) -> str:
 
-    if system_message != "": 
-        messages = [ {"role": "system", "content": system_message}]
-    else:
-        messages = [] # empty context
 
     article = pdf2txt(PDFBytes)
-    query = prompt_template.format(article=article)
+    text_chunks = split_text(article)
+    response_messages = []
+    for chunk in text_chunks:
+        if system_message != "":
+            messages = [{"role": "system", "content": system_message}]
+        else:
+            messages = []  # empty context
+        query = prompt_template.format(article=chunk)
+        messages.append({"role": "system", "content": query})  # add the query to the context
+        print ('messages', messages)
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+        )
+        response_messages.append(completion.choices[0]["message"]["content"])
 
-    messages.append({"role":"system", "content":query}) # add the query to the context
-
-    completion = openai.ChatCompletion.create(
-        model= 'gpt-3.5-turbo',
-        messages=messages 
-    )
-
-    return completion.choices[0]["message"]["content"]
+    return " ".join(response_messages)
